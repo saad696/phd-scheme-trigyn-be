78d03d28c3ae011cc1b86053db47c28b8b584951
Saad Shaikh <shksaad911@gmail.com>
2022-02-21 21:04:00 +0530

    refining create user

--- controllers/adminUser.controller.js
+++ controllers/adminUser.controller.js
@@ -5,67 +5,71 @@ const { validationResult } = require("express-validator");
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
 
-const findUser = async (mobilenumber) => {
-  const user = await adminUser.findOne({ mobilenumber: mobilenumber });
-  console.log("this is user :", user);
-  if (user) {
-    return true;
-  } else {
-    return false;
-  }
+const findUser = async (mobileNumber) => {
+    const user = await adminUser.findOne({ mobileNumber: mobileNumber });
+    console.log("this is user :", user);
+    if (user) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 const createUser = async (req, res) => {
-  const errors = validationResult(req);
+    const errors = validationResult(req);
 
-  if (!errors.isEmpty()) {
-    const error = new Error("Validation failed");
-    error.statusCode = 400;
-    error.data = errors.array();
-    return res.status(400).json(error);
-  }
+    if (!errors.isEmpty()) {
+        const error = new Error("Validation failed");
+        error.statusCode = 400;
+        error.data = errors.array();
+        return res.status(400).json(error);
+    }
 
-  const { mobilenumber } = req.body;
+    const { mobileNumber } = req.body;
 
-  const findForSameUser = await findUser(mobilenumber);
+    const findForSameUser = await findUser(mobileNumber);
 
-  if (findForSameUser) {
-    const error = new Error("User already present with this phone number");
-    error.statusCode = 409;
-    return res.status(409).json(error);
-  }
+    if (findForSameUser) {
+        const error = new Error("User already present with this phone number");
+        error.statusCode = 409;
+        return res.status(409).json(error);
+    }
 
-  try {
-    req.body.setpassword = await bcrypt.hash(req.body.setpassword, saltRounds);
-    const createUser = new adminUser(req.body);
-    createUser.save();
+    try {
+        req.body.setPassword = await bcrypt.hash(
+            req.body.setPassword,
+            saltRounds
+        );
+        const createUser = new adminUser(req.body);
+        createUser.save();
 
-    const toAuth = new authUser({
-      _id: createUser._id,
-      password: createUser.setpassword,
-      name: createUser.username,
-      email: "somerandom@gail.com", // this has to fixed but for now keep it like this
-    });
-    toAuth.save();
-    return res.status(200).json(createUser);
-  } catch (err) {
-    return res.status(400).json(err);
-  }
+        const toAuth = new authUser({
+            _id: createUser._id,
+            password: createUser.setPassword,
+            name: createUser.username,
+            email: "somerandom@gail.com", // this has to fixed but for now keep it like this
+        });
+        toAuth.save();
+        return res.status(200).json({
+            message: `Account for ${createUser.username} has been created.`,
+        });
+    } catch (err) {
+        return res.status(400).json(err);
+    }
 };
 
 const getRoles = async (req, res) => {
-  const { id } = req.params;
-  const data = await Roles.find(
-    { admin: id, status: "active" },
-    { _id: 1, role: 1 }
-  );
-  if (data.length > 0) {
-    return res.status(200).json(data);
-  } else {
-    const error = new Error("No Data");
-    error.statusCode = 400;
-    return res.status(400).json(error);
-  }
+    const data = await Roles.find(
+        { admin: req.userId, status: "active" },
+        { _id: 1, role: 1 }
+    );
+    if (data.length > 0) {
+        return res.status(200).json(data);
+    } else {
+        const error = new Error("No Data");
+        error.statusCode = 400;
+        return res.status(400).json(error);
+    }
 };
 
 module.exports = { createUser, getRoles };
--- model/adminUser.model.js
+++ model/adminUser.model.js
@@ -1,13 +1,30 @@
 const mongose = require("mongoose");
 
-const adminUserModel = new mongose.Schema({
-  username: String,
-  status: String,
-  role: mongose.Schema.Types.ObjectId,
-  createdate: { type: Date, default: Date.now },
-  mobilenumber: String,
-  setpassword: String,
-  setusername: String,
-});
+const adminUserModel = new mongose.Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+        },
+        status: {
+            type: String,
+            required: true,
+        },
+        role: mongose.Schema.Types.ObjectId,
+        mobileNumber: {
+            type: Number,
+            required: true,
+        },
+        setPassword: {
+            type: String,
+            required: true,
+        },
+        setUsername: {
+            type: String,
+            required: true,
+        },
+    },
+    { timestamps: true }
+);
 const adminUser = mongose.model("adminusers", adminUserModel);
 module.exports = adminUser;
